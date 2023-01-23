aa43ea7b0a4cd5f521faaa4ced48b0f869ae79ae
Manoranjan <sahoomanoranjan626@gmail.com>
2023-01-23 14:44:25 +0530

    Added responsive design for projects

--- src/components/Skills/Frontend.jsx
+++ src/components/Skills/Frontend.jsx
@@ -5,7 +5,7 @@ const Frontend = () => {
     <div className="skills__content">
       <h3 className="skills__title">Frontend</h3>
       <div className="skills__box">
-      <div className="skills__group">
+        <div className="skills__group">
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
